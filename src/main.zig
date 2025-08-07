9bc42f41371d7d707a1a32be5e0976058f2c3812
krypek <krypek@tuta.io>
2025-08-07 18:06:00 +0200

    Add random point json generation

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,44 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
 
-pub fn main() !void {}
+pub fn main() !void {
+    try writeRandomPoints(1000, 2137);
+}
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn writeRandomPoints(point_pair_count: u64, seed: u64) !void {
+    var prng = std.Random.DefaultPrng.init(seed);
+    const rand = prng.random();
+
+    // const stdout_file = std.io.getStdOut().writer();
+    // var bw = std.io.bufferedWriter(stdout_file);
+    // defer bw.flush() catch unreachable;
+    // const writer = bw.writer();
+
+    const file = try std.fs.cwd().createFile(
+        "output.json",
+        .{ .read = false },
+    );
+    defer file.close();
+    const writer = file.writer();
+
+    _ = try writer.write(
+        \\{
+        \\  "pairs": [
+        \\
+    );
+
+    var i: usize = 0;
+    while (i < point_pair_count) : (i += 1) {
+        const x0 = 360 * rand.float(f64) - 180;
+        const y0 = 360 * rand.float(f64) - 180;
+        const x1 = 360 * rand.float(f64) - 180;
+        const y1 = 360 * rand.float(f64) - 180;
+
+        try writer.print("    {{ \"x0\": {d}, \"y0\": {d}, \"x1\": {d}, \"y1\": {d} }}\n", .{ x0, y0, x1, y1 });
+    }
+
+    _ = try writer.write(
+        \\  ]
+        \\}
+    );
 }
